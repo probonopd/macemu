d1360cf781238b35bdee4b8374a337573c9ff946
probonopd <probonopd@users.noreply.github.com>
2016-12-18 11:36:38 +0100

    Create AppImage.sh

---
+++ SheepShaver/src/Unix/Linux/AppImage.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+########################################################################
+# Package the binaries built on Travis-CI as an AppImage
+# By Simon Peter 2016
+# For more information, see http://appimage.org/
+########################################################################
+
+export ARCH=$(arch)
+
+APP=SheepShaver
+LOWERAPP=${APP,,}
+
+mkdir -p $HOME/$APP/$APP.AppDir/usr/
+
+cd $HOME/$APP/
+
+wget -q https://github.com/probonopd/AppImages/raw/master/functions.sh -O ./functions.sh
+. ./functions.sh
+
+cd $APP.AppDir
+
+sudo chown -R $USER /app/
+
+# SheepShaver had been installed to the PREFIX /shp before
+cp -r /shp/* ./usr/
+
+########################################################################
+# Copy desktop and icon file to AppDir for AppRun to pick them up
+########################################################################
+
+get_apprun
+wget -c "https://raw.githubusercontent.com/rpmfusion/SheepShaver/master/SheepShaver.desktop"
+wget -c "https://github.com/rpmfusion/SheepShaver/raw/master/SheepShaver.png"
+wget -c "https://raw.githubusercontent.com/rpmfusion/SheepShaver/master/SheepShaver.appdata.xml"
+
+########################################################################
+# Other appliaction-specific finishing touches
+########################################################################
+
+
+
+########################################################################
+# Copy in the dependencies that cannot be assumed to be available
+# on all target systems
+########################################################################
+
+copy_deps
+
+########################################################################
+# Delete stuff that should not go into the AppImage
+########################################################################
+
+# Delete dangerous libraries; see
+# https://github.com/probonopd/AppImages/blob/master/excludelist
+delete_blacklisted
+
+########################################################################
+# desktopintegration asks the user on first run to install a menu item
+########################################################################
+
+get_desktopintegration $LOWERAPP
+
+########################################################################
+# Determine the version of the app; also include needed glibc version
+########################################################################
+
+GLIBC_NEEDED=$(glibc_needed)
+VERSION=${RELEASE_VERSION}-glibc$GLIBC_NEEDED
+
+########################################################################
+# Patch away absolute paths; it would be nice if they were relative
+########################################################################
+
+find usr/ -type f -exec sed -i -e 's|/usr|././|g' {} \;
+find usr/ -type f -exec sed -i -e 's|/shp|././|g' {} \;
+find usr/ -type f -exec sed -i -e 's@././/bin/env@/usr/bin/env@g' {} \;
+
+########################################################################
+# AppDir complete
+# Now packaging it as an AppImage
+########################################################################
+
+cd .. # Go out of AppImage
+
+mkdir -p ../out/
+generate_type2_appimage
+
+########################################################################
+# Upload the AppDir
+########################################################################
+
+transfer ../out/*
+echo "AppImage has been uploaded to the URL above; use something like GitHub Releases for permanent storage"
